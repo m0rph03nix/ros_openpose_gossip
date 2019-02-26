88fcd38fbade1f3f3d96e1db853abef09eafebc3
Raphael LEBER <raphael.leber@cpe.fr>
2019-02-26 14:32:20 +0100

    fix confusion between limbs and body_part in getShirtRect method

--- ros_openpose_gossip_node/scripts/process/OpenPoseGossip.py
+++ ros_openpose_gossip_node/scripts/process/OpenPoseGossip.py
@@ -530,19 +530,19 @@ class OpenPoseGossip():
 
     def getShirtRect(self, body_part):
 
-        if ("L_Hip" in limbs['abs']) and ("R_Hip" in limbs['abs']) and ("Neck" in limbs['abs']) :
+        if (RawPoseIndex.L_Hip in body_part) and (RawPoseIndex.R_Hip in body_part) and (RawPoseIndex.Neck in body_part) :
             x = fabs( body_part[RawPoseIndex.L_Hip].x - body_part[RawPoseIndex.R_Hip].x )
             y = fabs( body_part[RawPoseIndex.Neck].y - body_part[RawPoseIndex.R_Hip].y )            
             middle_x = ( body_part[RawPoseIndex.L_Hip].x + body_part[RawPoseIndex.R_Hip].x ) / 2
             middle_y = (body_part[RawPoseIndex.Neck].y * 2 + body_part[RawPoseIndex.R_Hip].y + body_part[RawPoseIndex.L_Hip].y ) / 4
 
-        elif ("L_Hip" in limbs['abs']) and ("Neck" in limbs['abs']) :
+        elif (RawPoseIndex.L_Hip in body_part) and (RawPoseIndex.Neck in body_part) :
             x = fabs( body_part[RawPoseIndex.Neck].x - body_part[RawPoseIndex.L_Hip].x )
             y = fabs( body_part[RawPoseIndex.Neck].y - body_part[RawPoseIndex.L_Hip].y )            
             middle_x = ( body_part[RawPoseIndex.L_Hip].x + body_part[RawPoseIndex.Neck].x ) / 2
             middle_y = (body_part[RawPoseIndex.Neck].y + body_part[RawPoseIndex.L_Hip].y ) / 2
         
-        elif ("R_Hip" in limbs['abs']) and ("Neck" in limbs['abs']) :
+        elif (RawPoseIndex.R_Hip in body_part) and (RawPoseIndex.Neck in body_part) :
             x = fabs( body_part[RawPoseIndex.Neck].x - body_part[RawPoseIndex.R_Hip].x )
             y = fabs( body_part[RawPoseIndex.Neck].y - body_part[RawPoseIndex.R_Hip].y )               
             middle_x = ( body_part[RawPoseIndex.R_Hip].x + body_part[RawPoseIndex.Neck].x ) / 2
